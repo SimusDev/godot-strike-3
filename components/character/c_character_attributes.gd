0b2e8c6b1c15956fa1ae5f10a7027b8631d4ce4d
SimusDev <simusdev@mail.ru>
2026-04-06 19:49:33 +0300

    Character Attributes

---
+++ components/character/c_character_attributes.gd
@@ -0,0 +1,12 @@
+extends Node
+class_name C_CharacterAttributes
+
+@export var team: R_Team
+
+func _ready() -> void:
+	SimusNetVars.register(
+		self,
+		[
+			"team"
+		], SimusNetVarConfig.new().flag_mode_server_only().flag_replication()
+	)
