24d2a173f3ddd2ac57c36c1630bdae85143c4413
SimusDev <simusdev@mail.ru>
2026-04-06 19:47:27 +0300

    Teams

---
+++ components/teams/r_team.gd
@@ -0,0 +1,26 @@
+extends Resource
+class_name R_Team
+
+const META: StringName = "R_Team"
+
+@export var name: StringName = ""
+@export var color: Color = Color.WHITE
+
+func set_in(object: Object) -> R_Team:
+	object.set_meta(META, object)
+	return self
+
+static func find_in(object: Object) -> R_Team:
+	if object.has_meta(META):
+		return object.get_meta(META)
+	return null
+
+static func find_above(from: Node) -> R_Team:
+	if from == SimusDev.get_tree().root:
+		return null
+	
+	var team: R_Team = find_in(from)
+	if team:
+		return team
+	
+	return find_above(from.get_parent())
