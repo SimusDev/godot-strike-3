7c211e0059bff445f37d9514bf8b6c3c996e9720
zxcvlad78 <danyaps@mail.ru>
2026-04-07 00:22:25 +0300

    SEX

---
+++ prefabs/character/model/skeleton/animated_model.gd
@@ -0,0 +1,30 @@
+@tool
+extends W_AnimatedModel3D
+
+@export var movement:W_FPCSourceLikeMovement
+
+var actor_velocity
+var blend_position
+
+var state_machine_playback:AnimationNodeStateMachinePlayback
+
+
+func _ready() -> void:
+	movement.state_machine.state_enter.connect(_on_state_enter)
+	movement.state_machine.state_exit.connect(_on_state_exit)
+	state_machine_playback = tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
+
+
+func _on_state_enter(state:SD_State):
+	state_machine_playback.travel(state.name)
+
+func _on_state_exit(state:SD_State):
+	pass
+
+func _physics_process(_delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
+	actor_velocity = movement.actor.velocity.normalized() * movement.actor.transform.basis
+	blend_position = Vector2(actor_velocity.x, -actor_velocity.z)
+
+	tree.set("parameters/StateMachine/%s/blend_position" % state_machine_playback.get_current_node(), blend_position)
