52c8fa3b43ab4920e522c4ead9a01ae5b2dcb84b
SimusDev <simusdev@mail.ru>
2026-04-08 21:16:21 +0300

    Update gameobjects_loading.gd

--- scenes/gameobjects_loading.gd
+++ scenes/gameobjects_loading.gd
@@ -3,4 +3,9 @@ extends Control
 func _ready() -> void:
 	await s_GameObjects.load_all()
 	await get_tree().process_frame
-	get_tree().change_scene_to_file("res://game.tscn")
+	WorkerThreadPool.add_task(_load_scene_threaded)
+
+func _load_scene_threaded() -> void:
+	var scene: PackedScene = load("res://game.tscn")
+	get_tree().change_scene_to_packed.call_deferred(scene)
+	
